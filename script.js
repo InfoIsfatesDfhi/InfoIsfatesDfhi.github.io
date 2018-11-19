b0cab80a358894fab1f6ece9d502b79043d7c28b
johann_chopin <31794680+johannchopin@users.noreply.github.com>
2018-11-19 23:40:39 +0100

    Create div for the dates,  add month map

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const MONTH = new Map([
+	["01", "Jan"],
+	["02", "Feb"],
+	["03", "Mar"],
+	["04", "Apr"],
+	["05", "May"],
+	["06", "Jun"],
+	["07", "Jul"],
+	["08", "Aug"],
+	["09", "Sep"],
+	["10", "Oct"],
+	["11", "Nov"],
+	["12", "Dec"]
+]);
+
 
 var displayedData = document.getElementById("htw");
 var navButton = document.getElementById("nav").children;
@@ -19,7 +34,7 @@ function replaceContent(button, contentToReplace, newContent) {
 }
 
 function creatTable(datas){
-    let container = document.createElement('table');
+    let container = document.getElementById("datesCtn");
 	let explodedDatas = datas.split("\n");
 	for (let i = 0; i < explodedDatas.length; i++){
 		if (explodedDatas[i].length > 0){
@@ -29,16 +44,28 @@ function creatTable(datas){
 	return container;
 }
 function buildTable(data, index){
-    let dataContainer = document.createElement('tr');
+    let dataContainer = document.createElement("div");
+    let dates = document.createElement("div");
+    let text = document.createElement("div")
+    
+    dataContainer.setAttribute("id", "dates");
+    dates.setAttribute("id", "datesString");
+    text.setAttribute("id", "text");
+
     let dateElement = data[index].split(":")[0];
-    let dates = document.createElement('th');
-    dates.innerHTML= dateElement;
+    dateElement = dateElement.split(" ");
+    dayName = dateElement[0];
+    fullDate = dateElement[1].split(".");
+    day = fullDate[0];
+    month = MONTH.get(fullDate[1]);
+    year = fullDate[2];
+    dates.innerHTML=  "<p>" + month +  " " + year + "</p>" + "<p>" + day + "</p>" + "<p>" + dayName + "</p>";
     let textElement = data[index].split(":")[1];
-    let text = document.createElement('td')
     text.innerHTML = textElement;
     dataContainer.appendChild(dates);
     dataContainer.appendChild(text);
     return dataContainer
 }
 
-document.getElementById("dates").appendChild(creatTable(datas));
+// Add all the dates in the dates container
+creatTable(dates);
