b96a61600828faaeb81e72c13bf7858eda02e6b4
johann_chopin <31794680+johannchopin@users.noreply.github.com>
2018-11-20 10:52:33 +0100

    Add some commentar

--- dates.js
+++ dates.js
@@ -1,3 +1,14 @@
+/*
+* Insert new date in the js variable bellow
+* 1 line for each date
+* Please order the date from the nearest to the latest
+*
+* Date have to be to this form:
+* [day] [date] : [text]
+*
+*/
+
+
 var dates = `
 
 
