e920520e649f192ecbe3a9441ab7efc75177feff
Lyokolux <lyokolux@protonmail.com>
2019-01-08 14:47:32 +0100

    Update dates.js
    Ajout du cours de Mensh Maschine Schnittstelle

--- dates.js
+++ dates.js
@@ -15,6 +15,8 @@ Wednesday 23.01.2019 : Klausur Deutsch
 
 Wednesday 06.02.2019 : Klausur Englisch + Klausur EI
 
+Tuesday 12.02.2018 : Cours Mensch Maschine Schnittstelle
+
 Monday 18.02.2019 : Klausur JavaScript 10h
 
 Monday 25.02.2019 : Klausur Programmierung I
