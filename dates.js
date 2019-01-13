28a697f1538bfec8d840b93abab6d716bf770e0e
johann_chopin <31794680+johannchopin@users.noreply.github.com>
2019-01-13 13:26:40 +0100

    Update dates.js

--- dates.js
+++ dates.js
@@ -17,13 +17,11 @@ Wednesday 06.02.2019 : Klausur Englisch + Klausur EI
 
 Tuesday 12.02.2018 : Cours Mensch Maschine Schnittstelle
 
-Monday 07.03.2019 : Klausur JavaScript 10h
-
 Monday 25.02.2019 : Klausur Programmierung I
 
-
-
 Wednesday 06.03.2019 : Klausur Datenbanken
+
+Thursday 07.03.2019 : Klausur JavaScript 10h
 
 Friday 29.03.2019 : Rendre rapport individuel UX
 
