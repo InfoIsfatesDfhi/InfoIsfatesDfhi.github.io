4628d9d4a07b5145f41d8d33687c1a7e32b8cc17
Lyokolux <lyokolux@protonmail.com>
2019-04-25 23:14:55 +0200

    Update dates.js
    Remove past dates

--- dates.js
+++ dates.js
@@ -9,20 +9,11 @@
 *
 */
 
-var dates = `
+/*
+* Examples :
+* Wednesday 06.02.2019 : Klausur Englisch + Klausur EI
+* Tuesday 12.02.2018 : Cours Mensch Maschine Schnittstelle
+* Monday 18.02.2019 : Klausur JavaScript 10h
+*/
 
-Wednesday 23.01.2019 : Klausur Deutsch
-
-Wednesday 06.02.2019 : Klausur Englisch + Klausur EI
-
-Tuesday 12.02.2018 : Cours Mensch Maschine Schnittstelle
-
-Monday 18.02.2019 : Klausur JavaScript 10h
-
-Monday 25.02.2019 : Klausur Programmierung I
-
-Wednesday 06.03.2019 : Klausur Datenbanken
-
-Friday 29.03.2019 : Rendre rapport individuel UX
-
-`
+var dates = ``
