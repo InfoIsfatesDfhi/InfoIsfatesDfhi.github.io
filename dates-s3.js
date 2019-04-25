c18e753468ba199cec6498a95945df8edf351cd9
Lyokolux <lyokolux@protonmail.com>
2019-04-25 23:23:35 +0200

    Add a new semester

---
+++ dates-s3.js
@@ -0,0 +1,28 @@
+/*
+*
+* Insert new date in the js variable bellow
+* 1 line for each date
+* Please order the date from the nearest to the latest
+*
+* Date have to be to this form:
+* [day] [date] : [text]
+*
+*/
+
+var dates = `
+
+Wednesday 23.01.2019 : Klausur Deutsch
+
+Wednesday 06.02.2019 : Klausur Englisch + Klausur EI
+
+Monday 18.02.2019 : Klausur JavaScript 10h
+
+Monday 25.02.2019 : Klausur Programmierung I
+
+Friday 01.03.2019 : Abgabe Mensch/Maschine
+
+Wednesday 06.03.2019 : Klausur Datenbanken
+
+Friday 29.03.2019 : Rendre rapport individuel UX
+
+`
--- dates.js
+++ dates.js
@@ -7,22 +7,9 @@
 * Date have to be to this form:
 * [day] [date] : [text]
 *
+* Examples :
+* Wednesday 06.03.2019 : Klausur Datenbanken
+* Friday 29.03.2019 : Rendre rapport individuel UX
 */
 
-var dates = `
-
-Wednesday 23.01.2019 : Klausur Deutsch
-
-Wednesday 06.02.2019 : Klausur Englisch + Klausur EI
-
-Monday 18.02.2019 : Klausur JavaScript 10h
-
-Monday 25.02.2019 : Klausur Programmierung I
-
-Friday 01.03.2019 : Abgabe Mensch/Maschine
-
-Wednesday 06.03.2019 : Klausur Datenbanken
-
-Friday 29.03.2019 : Rendre rapport individuel UX
-
-`
+var dates = ``
